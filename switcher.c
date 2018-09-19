276ba84552313f511ca54975ab7055a05a262b77
tfabiha <tfabiha@stuy.edu>
2018-09-19 13:20:22 -0400

    adding hw

--- switcher.c
+++ switcher.c
@@ -4,6 +4,32 @@
 
 int main()
 {
+  srand(time(NULL));
+
   int ray[10];
-  /* srand(time(NULL)); */
+
+  for (int i = 0; i < 10; i++)
+  {
+    ray[i] = rand();
+  }
+  ray[9] = 0;
+
+  for (int i = 0; i < 10; i++)
+  {
+    printf("Element %d of the first array is %d.\n", i, ray[i]);
+  }
+
+  int ray2[10];
+  int *point = ray;
+
+  for (int i = 9; i >= 0; i--)
+  {
+    ray2[i] = *point;
+    point += 1;
+  }
+
+  for (int i = 0; i < 10; i++)
+  {
+    printf("Element %d of the second array is %d.\n", i, ray2[i]);
+  }
 }
